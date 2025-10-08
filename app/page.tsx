429ebbc924c4d0caf46d19f9bae206ac445369ed
FSA-22 <simeonstat@gmail.com>
2025-10-08 21:58:10 +0000

    setting up

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <section className="w-full h-full text-4xl font-bold text-blue-700">
+    <section className="bg-[#010d39] flex justify-center items-center w-full h-screen text-4xl font-bold text-blue-700">
       Tunhub Website
     </section>
   );
